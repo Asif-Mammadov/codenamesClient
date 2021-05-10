76eeda7d06305bab6ef4f0d4e748d8e76646d207
Eyvaz Ahmadzada <eyvaz.ahmedzade.12@gmail.com>
2021-05-10 18:46:44 +0400

    added: register page

---
+++ pages/register/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Auth from '../../app/components/layouts/Auth';
+import Register from '../../app/components/templates/Register';
+
+const RegisterPage = () => (
+  <Auth>
+    <Register />
+  </Auth>
+);
+
+export default RegisterPage;
